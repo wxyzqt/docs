a610d4cec1c6eaab0024e36f521d09a6dfc85e52
wxyzqt <wxyzqt@foxmail.com>
2025-10-12 17:27:55 +0800

    feat:add principle docs

--- docs/.vitepress/sidebars/princple.ts
+++ docs/.vitepress/sidebars/princple.ts
@@ -2,9 +2,17 @@
 
 export default [
   {
-    text: "计算机原理",
+    text: "计算机原理速通",
     collapsed: false,
-    items: [{ text: "位操作", link: "/bitOperate" }],
+    items: [
+      { text: "硬件", link: "/hardware" },
+      { text: "汇编", link: "/assembly" },
+      { text: "BIOS", link: "/bios" },
+      { text: "操作系统", link: "/os" },
+      { text: "字符编码", link: "/characterEncoding" },
+      { text: "数据结构和算法", link: "/algorithm" },
+      { text: "编程语言对比", link: "/programmingLanguage" },
+    ],
   },
   {
     text: "JavaScript相关",
